d8e2cb57799a4b64f38e81876e293fae04eee461
Dale Morgan <dalemorgan04@gmail.com>
2019-01-14 17:21:26 +0000

    sidebar

--- TasksAngular/ClientApp/app/modules/sidebar/sidebar.component.ts
+++ TasksAngular/ClientApp/app/modules/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
-import { sidebar } from '../shared/animations';
+import { sidebar, fadeIn } from '../shared/animations';
 import { SidebarService } from './sidebar.service';
 import { SidebarTab } from '../../models/sidebar.model';
 
@@ -7,14 +7,18 @@ import { SidebarTab } from '../../models/sidebar.model';
     selector: 'sidebar',
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss'],
-    animations: [ sidebar ]
+    animations: [ sidebar , fadeIn ]
 })
 export class SidebarComponent implements OnInit {
 
     public sidebarState: string = 'expanded';
     @HostBinding('class.minified') hostMinifiedClass: boolean;
 
+    //Tabs
+    public activeTab: SidebarTab = SidebarTab.thoughtsAdd;
     public availableTabs: SidebarTab[] = [SidebarTab.thoughtsAdd];
+    public thoughtsEditIsHidden: boolean = false;
+    public thoughtsAddIsHidden: boolean = false;
 
     constructor(private sidebarService: SidebarService) { }
 
